a879a5be7a9c76548f3662aa8be3d3429a488019
Binyamin Klein <bendklein1@gmail.com>
2023-02-25 23:44:07 +0200

    Added methods

--- Assignment1/src/primitives/Ray.java
+++ Assignment1/src/primitives/Ray.java
@@ -1,6 +1,34 @@
 package primitives;
 
+import java.util.Objects;
+
 public class Ray {
-    Point point;
-    Vector direction;
+    final Point point;
+    final Vector direction;
+
+    public Ray(Point point, Vector direction) {
+        this.point = point;
+        this.direction = direction.normalize();
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public Vector getDirection() {
+        return direction;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || !(obj instanceof Ray)) return false;
+        Ray ray = (Ray) obj;
+        return (ray.point.equals(this.point) && ray.direction.equals(this.direction));
+    }
+
+    @Override
+    public String toString() {
+        return "point = " + point + ", direction = " + direction;
+    }
 }
