830a70f644d547d83298dbb49a39f62ffc2e3a55
Binyamin Klein <bendklein1@gmail.com>
2023-02-25 23:32:55 +0200

    Added Vector Constructors

--- Assignment1/src/primitives/Vector.java
+++ Assignment1/src/primitives/Vector.java
@@ -1,5 +1,23 @@
 package primitives;
 
 public class Vector extends Point {
-    Point endpoint;
+
+    public Vector(Double3 endpoint){
+        super(endpoint);
+
+        Double3 zero = new Double3(0,0,0);
+        if (this.equals(zero)){
+            throw new IllegalArgumentException("Cannot create a zero vector.");
+        }
+    }
+
+    public Vector(double num1, double num2, double num3){
+        super(num1,num2,num3);
+
+        Double3 zero = new Double3(0,0,0);
+        if (this.equals(zero)){
+            throw new IllegalArgumentException("Cannot create a zero vector.");
+        }
+    }
+
 }
