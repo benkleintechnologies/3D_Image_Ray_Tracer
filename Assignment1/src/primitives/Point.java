589816dad5a588b9d07cfeb6621c1df5ff2bcb79
Binyamin Klein <bendklein1@gmail.com>
2023-02-25 23:12:01 +0200

    Updated Point class

--- Assignment1/src/primitives/Point.java
+++ Assignment1/src/primitives/Point.java
@@ -1,5 +1,51 @@
 package primitives;
 
+/**
+ * Class Point is the basic class representing a point in Cartesian
+ * 3-Dimensional coordinate system.
+ * @author Eli Hawk and Binyamin Klein
+ * 563385586 & 576708589
+ */
 public class Point {
-    Double3 point;
+    /** Point of type Double3 */
+    final Double3 point;
+
+    /**
+     * Constructor to initialize Point based on Double3
+     * @param point
+     */
+    public Point(Double3 point) {
+        this.point = point;
+    }
+
+    /**
+     * Constructor to initialize Point based on two numbers of type double
+     * @param num1 first point
+     * @param num2 second point
+     */
+    public Point(double num1, double num2){
+        this.point = new Double3(num1, num2, 0);
+    }
+
+    /**
+     * Getter for point variable
+     * @return point
+     */
+    public Double3 getPoint() {
+        return point;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Point)) return false;
+        Point other = (Point) obj;
+        return this.point.equals(other.point);
+    }
+
+    @Override
+    public String toString() {
+        return point.toString();
+    }
 }
