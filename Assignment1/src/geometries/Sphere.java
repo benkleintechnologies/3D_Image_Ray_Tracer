51c26b7f7e925ae686a2b6b965c3ead2ab6628ba
Eli Hawk <elihawk97@gmail.com>
2023-02-26 16:14:09 +0200

    Added Javadoc for the Sphere and Tube class

--- Assignment1/src/geometries/Sphere.java
+++ Assignment1/src/geometries/Sphere.java
@@ -2,20 +2,39 @@ package geometries;
 
 import primitives.Point;
 import primitives.Vector;
-
+/**
+ * Class Sphere is the basic class representing a sphere in the Cartesian
+ * 3-Dimensional coordinate system.
+ * @author Eli Hawk and Binyamin Klein
+ * 563385586 & 576708589
+ */
 public class Sphere implements Geometry{
     Point center;
     double radius;
 
+    /**
+     * Constructor for Sphere class that receives the circles center and radius
+     * to construct the circle object
+     * @param p the center of the circle
+     * @param r the radius of the circle
+     */
     public Sphere(Point p, double r){
         center = p;
         radius = r;
     }
 
+    /**
+     * Getter for the radius attribute of a sphere
+     * @return radius of the sphere
+     */
     public double getRadius() {
         return radius;
     }
 
+    /**
+     * Getter for the center point attribute of a sphere
+     * @return center point of the sphere
+     */
     public Point getCenter() {
         return center;
     }
