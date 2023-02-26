3cebb186685b26db976b5b8c5fb0664cc194b95e
Binyamin Klein <bendklein1@gmail.com>
2023-02-26 16:14:15 +0200

    Created Geometry Interface and shape classes (Plane, Triangle, Sphere, Tube, Cylinder) and fixed Polygon

--- Assignment1/src/geometries/Cylinder.java
+++ Assignment1/src/geometries/Cylinder.java
@@ -2,14 +2,29 @@ package geometries;
 
 import primitives.Ray;
 
+/**
+ * Class Cylinder is the class representing a cylinder (as a type of Tube) in Cartesian 3-Dimensional coordinate system.
+ * @author Eli Hawk and Binyamin Klein
+ * 563385586 & 576708589
+ */
 public class Cylinder extends Tube{
     double height;
 
+    /**
+     * Constructor for Cylinder which calls super constructor and sets height
+     * @param axis of tube
+     * @param r radius of the tube
+     * @param h height
+     */
     public Cylinder(Ray axis, double r, double h) {
         super(axis, r);
         height = h;
     }
 
+    /**
+     * Getter for height
+     * @return height
+     */
     public double getHeight() {
         return height;
     }
