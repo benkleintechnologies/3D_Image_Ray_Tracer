d0eb4b19d17f10680b1fe643f631d06fc6d58d2d
Binyamin Klein <bendklein1@gmail.com>
2023-02-28 18:55:22 +0200

    Added PointTest

---
+++ unittets/primitives/PointTest.java
@@ -0,0 +1,36 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void getPoint() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void distanceSquared() {
+    }
+
+    @Test
+    void distance() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
