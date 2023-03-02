6b5c9489a6a9a66d94b585ae5a9dcf464c43a6b5
Binyamin Klein <bendklein1@gmail.com>
2023-03-02 17:17:00 +0200

    Add PointTest javadocs

--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -2,34 +2,81 @@ package primitives;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Unit tests for primitives.Point class
+ * @author Eli Hawk and Binyamin Klein
+ * 563385586 & 576708589
+ */
 class PointTest {
 
+    /** Test method for {@link primitives.Point#Point(Double3)}. */
     @Test
-    void getPoint() {
+    public void testConstructorDouble3() {
     }
 
+    /** Test method for {@link primitives.Point#Point(double, double, double)}. */
+    @Test
+    public void testConstructorThreeDoubles() {
+    }
+
+    /**
+     * Test method for {@link primitives.Point#subtract(primitives.Point)}.
+     */
     @Test
     void subtract() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
     }
 
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Point)}.
+     */
     @Test
     void add() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
     }
 
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Point)}.
+     */
     @Test
     void distanceSquared() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
     }
 
+    /**
+     * Test method for {@link primitives.Point#distance(primitives.Point)}.
+     */
     @Test
     void distance() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
     }
 
+    /**
+     * Test method for {@link primitives.Point#equals(Object)}.
+     */
     @Test
     void testEquals() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
     }
 
+    /**
+     * Test method for {@link primitives.Point#toString()}.
+     */
     @Test
     void testToString() {
     }
