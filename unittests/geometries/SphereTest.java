a4d7a33fa80e349afb5bd16cc996454fd0b866dc
Eli Hawk <elihawk97@gmail.com>
2023-03-23 16:27:55 +0200

    updated name of the test cases in the Sphere class. to include the name "test" at the beginning of testing the method section

--- unittests/geometries/SphereTest.java
+++ unittests/geometries/SphereTest.java
@@ -3,6 +3,9 @@ package geometries;
 import org.junit.jupiter.api.Test;
 import primitives.Point;
 import primitives.Vector;
+import primitives.Ray;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static primitives.Util.isZero;
@@ -12,16 +15,16 @@ import static primitives.Util.isZero;
  * @author Eli Hawk and Binyamin Klein
  * 563385586 & 576708589
  */
-class SphereTest {
+class testSphereTest {
 
     /** Test method for {@link Sphere#getRadius()} . */
     @Test
-    void getRadius() {
+    void testgetRadius() {
     }
 
     /** Test method for {@link Sphere#getCenter()} . */
     @Test
-    void getCenter() {
+    void testgetCenter() {
     }
 
     /** Test method for {@link Sphere#toString()} . */
@@ -31,7 +34,7 @@ class SphereTest {
 
     /** Test method for {@link Sphere#getNormal(Point)} . */
     @Test
-    void getNormal() {
+    void testgetNormal() {
         // ============ Equivalence Partitions Tests ==============
         // TC01: Check that normal
         Sphere sph = new Sphere(new Point(0,0,0), 5);
@@ -43,5 +46,44 @@ class SphereTest {
         assertEquals(1, result.length(), 0.00000001, "Sphere's normal is not a unit vector");
         // Check that vector returned is normal to the sphere at the point (i.e. given point minus center, normalized)
         assertEquals(new Point(0,0,1), result, "Result is not the normal to the sphere at the point");
+    }
+
+    @Test
+    public void testFindIntersections() {
+        Sphere sphere = new Sphere(1d, new Point (1, 0, 0));
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Ray's line is outside the sphere (0 points)
+        assertNull(sphere.findIntersections(new Ray(new Point(-1, 0, 0), new Vector(1, 1, 0))),
+                "Ray's line out of sphere");
+        // TC02: Ray starts before and crosses the sphere (2 points)
+        Point p1 = new Point(0.0651530771650466, 0.355051025721682, 0);
+        Point p2 = new Point(1.53484692283495, 0.844948974278318, 0);
+        List<Point> result = sphere.findIntersections(new Ray(new Point(-1, 0, 0),
+                new Vector(3, 1, 0)));
+        assertEquals(2, result.size(), "Wrong number of points");
+        if (result.get(0).getX() > result.get(1).getX())
+            result = List.of(result.get(1), result.get(0));
+        assertEquals(List.of(p1, p2), result, "Ray crosses sphere");
+        // TC03: Ray starts inside the sphere (1 point)
+
+        // TC04: Ray starts after the sphere (0 points)
+
+        // =============== Boundary Values Tests ==================
+        // **** Group: Ray's line crosses the sphere (but not the center)
+        // TC11: Ray starts at sphere and goes inside (1 points)
+        // TC12: Ray starts at sphere and goes outside (0 points)
+        // **** Group: Ray's line goes through the center
+        // TC13: Ray starts before the sphere (2 points)
+        // TC14: Ray starts at sphere and goes inside (1 points)
+        // TC15: Ray starts inside (1 points)
+        // TC16: Ray starts at the center (1 points)
+        // TC17: Ray starts at sphere and goes outside (0 points)
+        // TC18: Ray starts after sphere (0 points)
+        // **** Group: Ray's line is tangent to the sphere (all tests 0 points)
+        // TC19: Ray starts before the tangent point
+        // TC20: Ray starts at the tangent point
+        // TC21: Ray starts after the tangent point
+        // **** Group: Special cases
+        // TC19: Ray's line is outside, ray is orthogonal to ray start to sphere's center line
     }
 }
