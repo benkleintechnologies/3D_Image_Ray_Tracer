d2788bd20ed48ab5837c6a4158c00f237d45eb4d
Binyamin Klein <bendklein1@gmail.com>
2023-04-04 14:40:11 -0400

    Added GeometriesTests class and list of tests

---
+++ unittests/geometries/GeometriesTests.java
@@ -0,0 +1,33 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+import primitives.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for geometries.Geometries class
+ * @author Eli Hawk and Binyamin Klein
+ * 563385586 & 576708589
+ */
+public class GeometriesTests {
+
+    /** Test method for {@link Geometries#findIntersections(Ray)}. */
+    @Test
+    void testFindIntersections() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Several (but not all) geometries intersect
+
+
+        // ============ Boundary Value Tests ==============
+        // TC11: Empty geometries collection
+
+        // TC12: No intersection point
+
+        // TC13: Only one geometry intersects
+
+        // TC14: Several (but not all) geometries intersect
+
+        // TC15: All geometries intersect
+    }
+}
