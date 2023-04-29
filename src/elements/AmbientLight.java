e8d7c8342b8b7e17b35abd61ea05d5bada3cb43e
Binyamin Klein <bendklein1@gmail.com>
2023-04-29 23:37:08 +0300

    Implemented AmbientLight class Co-authored-by: Eli Hawk <elihawk97@gmail.com>

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -6,4 +6,30 @@ package elements;
  * 563385586 & 576708589
  */
 public class AmbientLight {
+    Color intensity;
+
+    /**
+     * Constructor which calculates and sets the final value of the color
+     * @param color the initial color of the light
+     * @param attenuation the attenuation factor of the original light
+     */
+    public AmbientLight(Color color, Double3 attenuation){
+        intensity = color.scale(attenuation);
+    }
+
+    /**
+     * Default constructor which sets the color to be black
+     */
+    public AmbientLight(){
+        intensity = Color.BLACK;
+    }
+
+    /**
+     * Getter for the color
+     * @return intensity
+     */
+    public Color getIntensity(){
+        return intensity;
+    }
+
 }
