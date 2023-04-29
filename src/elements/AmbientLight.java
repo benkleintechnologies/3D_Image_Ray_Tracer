d689d19068ea4e70edd3f193cc74e3b93b2899b5
Binyamin Klein <bendklein1@gmail.com>
2023-04-29 23:26:14 +0300

    Created and moved files Co-authored-by: Eli Hawk <elihawk97@gmail.com>

---
+++ src/elements/AmbientLight.java
@@ -0,0 +1,9 @@
+package elements;
+
+/**
+ * Class AmbientLight represents the ambient light in the scene in a 3D space.
+ * @author Eli Hawk and Binyamin Klein
+ * 563385586 & 576708589
+ */
+public class AmbientLight {
+}
