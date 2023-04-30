a3d3b00add3c53b82990846fa77649047d3e5291
Eli Hawk <elihawk97@gmail.com>
2023-04-30 22:49:26 +0300

    Added TraceRay and calcColor method to the RayTraceBasic class. And made slight update to test case, to fix order of parameters passed to it.
    Co-authored-by: Binyamin Klein <bendklein1@gmail.com>

--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -1,8 +1,11 @@
 package renderer;
 
 import primitives.Color;
+import primitives.Point;
 import primitives.Ray;
 import scene.Scene;
+
+import java.util.List;
 
 /**
  * Class RayTracerBasic which inherits from RayTracerBase
@@ -19,8 +22,23 @@ public class RayTracerBasic extends RayTracerBase {
         super(scene);
     }
 
+    /**
+     * Calculate the color of the pixel
+     * @param p the point that we wish to get its color
+     * @return the color of the point
+     */
+    private Color calcColor(Point p){
+        return scene.ambientLight.getIntensity();
+    }
     @Override
     public Color traceRay(Ray ray) {
-        return null;
+        List<Point> intersections = scene.geometries.findIntersections(ray);
+
+        if(intersections == null){
+            return scene.background;
+        }
+
+        Point closest = ray.findClosestPoint(intersections);
+        return calcColor(closest);
     }
 }
