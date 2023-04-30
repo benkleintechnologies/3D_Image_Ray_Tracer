df48cfdc7e8e021b0e1e3b4b222542494d4b9777
Binyamin Klein <bendklein1@gmail.com>
2023-04-30 22:29:51 +0300

    Implemented RayTracerBasic

--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -1,4 +1,26 @@
 package renderer;
 
-public class RayTracerBasic {
+import primitives.Color;
+import primitives.Ray;
+import scene.Scene;
+
+/**
+ * Class RayTracerBasic which inherits from RayTracerBase
+ * @author Eli Hawk and Binyamin Klein
+ * 563385586 & 576708589
+ */
+public class RayTracerBasic extends RayTracerBase {
+
+    /**
+     * Constructor for RayTracerBase
+     * @param scene the scene
+     */
+    public RayTracerBasic(Scene scene) {
+        super(scene);
+    }
+
+    @Override
+    public Color traceRay(Ray ray) {
+        return null;
+    }
 }
