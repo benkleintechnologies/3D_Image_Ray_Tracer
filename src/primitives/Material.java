8e9da299c3ecbf56b6f243779d1ea8fcb01d1d78
Binyamin Klein <bendklein1@gmail.com>
2023-05-14 23:31:27 +0300

    Added kT and kR Co-authored-by: Eli Hawk <elihawk97@gmail.com>

--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -10,6 +10,10 @@ public class Material {
     public Double3 kD = new Double3(0);
     //kS - specular attenuation coefficient
     public Double3 kS = new Double3(0);
+    //kT - transparency  coefficient
+    public Double3 kT = new Double3(0);
+    //kR - reflection coefficient
+    public Double3 kR = new Double3(0);
     //nShininess - shininess level
     public int nShininess = 0;
 
@@ -30,6 +34,46 @@ public class Material {
      */
     public Material setKd(Double kD){
         this.kD = new Double3(kD);
+        return this;
+    }
+
+    /**
+     * Setter for kT which receives Double3
+     * @param kT transparency coefficient
+     * @return this material object
+     */
+    public Material setKt(Double3 kT){
+        this.kT = kT;
+        return this;
+    }
+
+    /**
+     * Setter for kT which receives Double
+     * @param kT transparency coefficient
+     * @return this material object
+     */
+    public Material setKt(Double kT){
+        this.kT = new Double3(kT);
+        return this;
+    }
+
+    /**
+     * Setter for kR which receives Double3
+     * @param kR reflection coefficient
+     * @return this material object
+     */
+    public Material setKr(Double3 kR){
+        this.kR = kR;
+        return this;
+    }
+
+    /**
+     * Setter for kR which receives Double
+     * @param kR reflection coefficient
+     * @return this material object
+     */
+    public Material setKr(Double kR){
+        this.kR = new Double3(kR);
         return this;
     }
 
